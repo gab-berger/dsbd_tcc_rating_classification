85e155699bb490a11556db854003ef6792ae8dbf
Gabriel Berger <gabriel.fberger@gmail.com>
2024-10-17 10:25:13 -0300

    [ADD] multiple models w/ txt output

--- test_ollama_bash_coms.py
+++ test_ollama_bash_coms.py
@@ -5,17 +5,28 @@ from time import time
 def message_ollama(prompt, model='llama3.2'):
     command = ['ollama', 'run', model]
     start_time = time()
+    
     try:
         resultado = subprocess.run(command, input=prompt, capture_output=True, text=True)
         if resultado.returncode != 0:
             print(f"Ocorreu um erro ao executar o Ollama:\n{resultado.stderr}")
         else:
-            print(command[2],'- ',round(time() - start_time,1),'segundos:\n')
-            print(resultado.stdout)
+            result = resultado.stdout
+            total_time = round(time() - start_time,1)
+            result_str = f'{model} - {total_time}s:\n{result}'
+            print(result_str)
     except Exception as e:
         print(f"Ocorreu um erro inesperado: {e}")
+    return result_str
 
 def main(): 
+    models = [
+        'stablelm2',
+        'llama3.2',
+        'llama3.1',
+        'stablelm2:12b',
+        ]
+    
     start_prompt = 'A seguir, te mando uma série de comentários de uma pesquisa organizaional, um a cada linha, e gostaria que você fizesse um resumo destes comentários:'
     end_prompt = 'Com base nestes comentários, faça um resumo, em português do Brasil, que contenha 2 parágrafos: o primeiro parágrafo resumindo os pontos positivos, e o segundo contendo os pontos negativos. Fique atento ao que foi escrito, utilize apenas isso como base, não adicione novas informações.'
 
@@ -34,10 +45,16 @@ def main():
     for n in range(n_comments):
         prompt = prompt+'\n'+comments[n]
     prompt = prompt + end_prompt
-    print(start_prompt,f'\n({n_comments} comentários)\n',end_prompt,'\n')
+    out_txt = f'{start_prompt}\n[{n_comments} comentários]\n{end_prompt}\nTamanho do prompt: {len(prompt)}\n'
+    print(out_txt)
 
-    message_ollama(prompt, 'llama3.2')
-    message_ollama(prompt, 'llama3.1')
+    for model in models:
+        result = message_ollama(prompt, model)
+        out_txt = '\n'+out_txt+result
+
+    file_name = "output.txt"
+    with open(file_name, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(out_txt)
 
 if __name__ == '__main__':
     main()
